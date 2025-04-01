fb36dc1658d746710625c9ffdb2ead96d1610eab
fatih <huze.ozr@gmail.com>
2025-04-01 08:07:38 -0400

    fixed bug

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'expo-router';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, onIdTokenChanged } from 'firebase/auth';
 import Login from './(auth)/login';
 import { auth } from './firebaseConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
   const router = useRouter();
   const [checkingAuth, setCheckingAuth] = useState(true);
-
+  
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        router.replace('/(tabs)');
-      } else {
-        setCheckingAuth(false);
-        router.replace('/login');
+    
+    onIdTokenChanged(auth, async (user) => {
+      try {
+        if (user) {
+          const token = await user?.getIdToken();
+          await AsyncStorage.setItem('userToken', token!);
+          router.replace('/(tabs)');
+        } else {
+          await AsyncStorage.removeItem('userToken');
+          setCheckingAuth(false);
+          
+        }
+      } catch (error) {
+        console.error('Error in onIdTokenChanged:', error);
       }
     });
 
